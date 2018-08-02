eecd77befaa3d31e65d3dedd56568d63cb2873d2
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-02 08:00:56 -0300

    Criação do componente Menu e Sidebar

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,9 +1,11 @@
 import '../common/template/dependencies'
 import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
 import React from 'react'
 
 export default props => ('',
     <div className='wrapper'>
         <Header />
+        <Sidebar />
     </div>
 )
