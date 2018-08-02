a451b88561faab19eb498bb00dd53e1de74d2bd1
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-02 08:20:56 -0300

    Rodapé

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => ('',
+
+    <footer className="main-footer">
+        <strong>Copyright &copy; Guiljs 2018</strong><br />
+        <a href="#">guiljs.com</a>
+    </footer>
+)
