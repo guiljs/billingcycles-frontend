8afaea07f02efddca42ea61b2461c0dd6c810de4
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-20 08:44:04 -0300

    Integração do Dashboard com o Redux

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
@@ -7,14 +8,15 @@ import Row from '../common/layout/row';
 
 class Dashboard extends Component {
     render() {
+        const { credit, debt } = this.props.summary; //Destructure (extrair)
         return (
             <div>
                 <ContentHeader title='Dashboard' subtitulo='Versão 1.0' />
                 <Row>
                     <Content>
-                        <ValueBox cols='12 4' color='green' value='R$ 10,00' text='Créditos' icon='plus' />
-                        <ValueBox cols='12 4' color='red' value='R$ 10,00' text='Débitos' icon='minus' />
-                        <ValueBox cols='12 4' color='blue' value='R$ 0,00' text='Total' icon='money' />
+                        <ValueBox cols='12 4' color='green' value={`R$ ${credit}`} text='Créditos' icon='plus' />
+                        <ValueBox cols='12 4' color='red' value={`R$ ${debt}`} text='Débitos' icon='minus' />
+                        <ValueBox cols='12 4' color='blue' value={`R$ ${credit - debt}`} text='Total' icon='money' />
                     </Content>
                 </Row>
             </div>
@@ -22,4 +24,6 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+// const mapStateToProps = state => ({ summary: state.dashboard.summary });
+
+export default connect(state => ({ summary: state.dashboard.summary }))(Dashboard); //Decorator (dentro do DashBoard retornado estará decorado um connect.)
---
+++ src/dashboard/dashboardReducer.js
@@ -0,0 +1,5 @@
+const INITIAL_STATE = { summary: { credit: 0, debt: 0 } };//Valor inicial da aplicação
+
+export default function (state = INITIAL_STATE, action) {
+    return state;
+}
