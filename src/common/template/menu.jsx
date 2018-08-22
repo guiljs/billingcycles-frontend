938b97c7cbc7e656e26a17454b80c7c426c7ff67
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-08-22 08:13:11 -0300

    Redux DevTools e Saldo no menu

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import MenuItem from './menuItem'
 import MenuTree from './menuTree'
 
-export default props => ('',
-    <ul className="sidebar-menu">
-        <MenuItem path='#/' label='Dashboard' icon='dashboard' />
-        <MenuTree label='Cadastro' icon='edit'>
-            <MenuItem path='#billingCycles' label='Ciclos de Pagamentos' icon='usd' />
-        </MenuTree>
-    </ul>
-)
+class Menu extends Component{
+    render(){
+        const { credit, debt } = this.props.summary; //Destructure (extrair)
+        const total = `Saldo = R$ ${credit - debt}`;
+
+        return(
+            <ul className="sidebar-menu">
+                <MenuItem path='#/' label='Dashboard' icon='dashboard' />
+                <MenuTree label='Cadastro' icon='edit'>
+                    <MenuItem path='#billingCycles' label='Ciclos de Pagamentos' icon='usd' />
+                </MenuTree>
+
+                <MenuItem path='#/' label={total} icon='money' />
+                
+                
+            </ul>
+
+        )
+    }
+}
+
+const mapStateToProps = state => ({ summary: state.dashboard.summary });
+export default connect(mapStateToProps) (Menu);
